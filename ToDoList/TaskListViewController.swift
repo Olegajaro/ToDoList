03f93c7739fbb1f7ce6c6633f4598fc13efb74b4
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-10-05 12:43:22 +0300

    The panel button element has been created, with the help of which the transition to the TaskViewController is made.

--- ToDoList/TaskListViewController.swift
+++ ToDoList/TaskListViewController.swift
@@ -16,9 +16,9 @@ class TaskListViewController: UITableViewController {
     }
     
     // метод для настройки Navigation Bar
-    // устанавливаем Title
-    // меняем стиль Navigation bar на Large titles
     private func setupNavigationBar() {
+        // устанавливаем Title
+        // меняем стиль Navigation bar на Large titles
         title = "Task List"
         navigationController?.navigationBar.prefersLargeTitles = true
         
@@ -41,11 +41,36 @@ class TaskListViewController: UITableViewController {
         navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
         navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         
+        // Добавляем кнопку в Navigation Bar для добавления задачи
+        // Для этого нужно у navigationItem вызвать свойство rightBarButtonItem, которое размещает итем справа
+        // и присваиваем ему экземпляр класса UIBarButtonItem
+        // в инициализатор класса передаем 3 параметра:
+        // barButtonSystemItem, отвечающий за предназначение кнопки, в нашем случае это add
+        // target, где будет вызываться данный экземпляр, передаем self, т.к. вызывается в этом же классе
+        // action, действие для кнопки, которое передается через селектор, в котором нужно указать @objc метод
+        // метод будет добавлять новую задачу addNewTask
+        // в этом варианте, этот метод будет служить переходом на другой ViewController, где мы будем создавать и добавлять задачу уже на главный экран
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        
+        navigationController?.navigationBar.tintColor = .white
         // Для того, чтобы Navigation Bar был покрашен во всех его состояниях,
         // нужно присвоить экземпляр класса UINavigationBarAppearance двум параметрам,
         // которые отвечают  за состояние Navigation Bar
         navigationController?.navigationBar.standardAppearance = navBarAppearance
         navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
+    
+    @objc private func addNewTask() {
+        // Для перехода на другой ViewController в методе нужно создать экземпляр этого ViewController.
+        // И чтобы его открыть нужно вызвать метод present, в который необходимо передать два параметра:
+        // первый параметр - это представление, которые мы хотим открыть, т.е наш TaskViewController
+        // второй параметр отвечает за анимацию, почти всегда ставится true, так как все происходит на глазах у пользователя
+        let taskVC = TaskViewController()
+        present(taskVC, animated: true)
     }
 }
 
---
+++ ToDoList/TaskViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TaskViewController.swift
+//  ToDoList
+//
+//  Created by Олег Федоров on 05.10.2021.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+
+}
